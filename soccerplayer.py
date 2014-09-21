04dbba5a202e83e2c9950b3cb8696e2761ba10c4
hoarf <alan.ficagna@gmail.com>
2014-09-21 14:28:58 -0300

    Add argument parsing

---
+++ soccerplayer.py
@@ -0,0 +1,11 @@
+import argparse
+from robotsoccer import SoccerClient
+
+def parse_host_port():
+    parser = argparse.ArgumentParser(description="A robotsoccer player")
+    parser.add_argument('--host', default='localhost')
+    parser.add_argument('--port', type=int, default=1024)
+    args = parser.parse_args()
+    return args.host, args.port
+
+print parse_host_port()

61e7396389ddca5fc59497262923b43deac39b5f
hoarf <alan.ficagna@gmail.com>
2014-09-21 16:35:30 -0300

    Increase interval resolution

--- fuzzy_ia.py
+++ fuzzy_ia.py
@@ -2,8 +2,11 @@
 import random
 import numpy as np
 import skfuzzy as fuzz
+import math
 
-n_domain = np.arange(-180,180,1)
+RESOLUTION = .5
+SPAN = 360/RESOLUTION
+n_domain = np.arange(-180,180,RESOLUTION)
 
 E_input = fuzz.trimf(n_domain, [-180, -180, 0])
 F_input = fuzz.trimf(n_domain, [-90, 0, 90])
@@ -14,17 +17,19 @@ F_output = fuzz.trimf(n_domain, [-180, 0, 180])
 D_output = fuzz.trimf(n_domain, [0, 180, 180])
 
 rel = lambda x1,x2: reduce(np.fmax,[
-        np.fmin(np.tile(np.min([E_input[x1],E_input[x2]]),360),F_output),
-        np.fmin(np.tile(np.min([E_input[x1],F_input[x2]]),360),E_output),
-        np.fmin(np.tile(np.min([E_input[x1],D_input[x2]]),360),E_output),
-        np.fmin(np.tile(np.min([F_input[x1],E_input[x2]]),360),D_output),
-        np.fmin(np.tile(np.min([F_input[x1],F_input[x2]]),360),F_output),
-        np.fmin(np.tile(np.min([F_input[x1],D_input[x2]]),360),E_output),
-        np.fmin(np.tile(np.min([D_input[x1],E_input[x2]]),360),D_output),
-        np.fmin(np.tile(np.min([D_input[x1],F_input[x2]]),360),D_output),
-        np.fmin(np.tile(np.min([D_input[x1],D_input[x2]]),360),F_output)
+        np.fmin(np.tile(np.min([E_input[x1],E_input[x2]]),SPAN),F_output),
+        np.fmin(np.tile(np.min([E_input[x1],F_input[x2]]),SPAN),E_output),
+        np.fmin(np.tile(np.min([E_input[x1],D_input[x2]]),SPAN),E_output),
+        np.fmin(np.tile(np.min([F_input[x1],E_input[x2]]),SPAN),D_output),
+        np.fmin(np.tile(np.min([F_input[x1],F_input[x2]]),SPAN),F_output),
+        np.fmin(np.tile(np.min([F_input[x1],D_input[x2]]),SPAN),E_output),
+        np.fmin(np.tile(np.min([D_input[x1],E_input[x2]]),SPAN),D_output),
+        np.fmin(np.tile(np.min([D_input[x1],F_input[x2]]),SPAN),D_output),
+        np.fmin(np.tile(np.min([D_input[x1],D_input[x2]]),SPAN),F_output)
 
 ])
 
 def decide(target_angle, ball_angle):
+    target_angle = math.floor(target_angle)
+    ball_angle = math.floor(ball_angle)
     return fuzz.defuzz(n_domain, rel(ball_angle,target_angle), 'centroid')

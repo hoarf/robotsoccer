a8654af26f1d065620f185af4ad77669211b2f22
hoarf <alan.ficagna@gmail.com>
2014-09-21 22:09:53 -0300

    Add unicycle model

--- fuzzy_ia.py
+++ fuzzy_ia.py
@@ -4,9 +4,10 @@ import numpy as np
 import skfuzzy as fuzz
 import math
 
-RESOLUTION = .5
+RESOLUTION = 1 
 SPAN = 360/RESOLUTION
 n_domain = np.arange(-180,180,RESOLUTION)
+print(SPAN)
 
 E_input = fuzz.trimf(n_domain, [-180, -180, 0])
 F_input = fuzz.trimf(n_domain, [-90, 0, 90])
@@ -26,10 +27,9 @@ rel = lambda x1,x2: reduce(np.fmax,[
         np.fmin(np.tile(np.min([D_input[x1],E_input[x2]]),SPAN),D_output),
         np.fmin(np.tile(np.min([D_input[x1],F_input[x2]]),SPAN),D_output),
         np.fmin(np.tile(np.min([D_input[x1],D_input[x2]]),SPAN),F_output)
-
 ])
 
 def decide(target_angle, ball_angle):
-    target_angle = math.floor(target_angle)
-    ball_angle = math.floor(ball_angle)
+    target_angle = int(math.floor(target_angle))
+    ball_angle = int(math.floor(ball_angle))
     return fuzz.defuzz(n_domain, rel(ball_angle,target_angle), 'centroid')
--- soccerplayer.py
+++ soccerplayer.py
@@ -22,16 +22,40 @@ print("Iniciando conexão com o host %s, na porta %s" %  (host,port))
 sc = SoccerClient()
 sc.connect(host, port)
 
+def normalize(angle):
+    if angle < -math.pi:
+        angle += 2*math.pi
+    elif angle > math.pi:
+        angle -= 2*math.pi
+    elif angle < -math.pi/2:
+        angle = -math.pi/2
+    elif angle > math.pi/2:
+        angle = math.pi/2
+    return angle
+
+
+def ball_angle():
+    n= math.degrees(normalize(sc.get_ball_angle()))
+    return n
+
+def target_angle():
+    n=  math.degrees(normalize(sc.get_target_angle()))
+    return n
+
 IA = {
     'dummy': lambda: angle_to_left_right_force(random.uniform(-math.pi,math.pi)),
     'ball_freak': lambda: angle_to_left_right_force(sc.get_ball_angle()),
-    'fuzzy': lambda: angle_to_left_right_force(fuzzy_ia.decide(math.degrees(sc.get_target_angle()),
-                                    math.degrees(sc.get_ball_angle()))),
+    'fuzzy': lambda: unicycle_model(fuzzy_ia.decide(target_angle(), ball_angle())),
     'fixed': lambda: (.5,1)
 }
 
 def angle_to_left_right_force(angle):
     return (math.cos(angle) - math.sin(angle), math.cos(angle)+math.sin(angle))
+
+def unicycle_model(angle):
+    angle = math.radians(angle)/4.0
+    print angle
+    return 0.5, 0.5+angle
 
 while True:
     ia_choice = IA[choice]()

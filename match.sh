10941f81ed78fda7740f27299cdae4f1b4a9f8ca
hoarf <alan.ficagna@gmail.com>
2014-11-25 00:12:34 -0200

    removed unused files

--- match.sh
+++
@@ -1,5 +0,0 @@
- #!/usr/sbin/bash
-
-cd ~/Downloads/ && wine ~/Downloads/soccerServer.exe > /dev/null &
-
-wine ~/Downloads/SoccerMatch.exe > /dev/null &
--- notas.md
+++
@@ -1,6 +0,0 @@
-Avaliação Futebol de Robôs (RN e SF):
-
-Enviar códigos, executáveis, relatórios
-
-renato.ppontes@gmail.com
-engel@inf.ufrgs.br 
